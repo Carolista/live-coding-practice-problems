257c91c6170c70ea8750bc6656ac04620b2ba3f3
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 16:53:34 -0500

    added second solution for halloween-candy

--- intermediate/halloween-candy/solution.js
+++ intermediate/halloween-candy/solution.js
@@ -1,5 +1,5 @@
 /* 
-  Solution
+  Solution 1
 */
 
 function howMuchCandy(weather, tempRange, avgKids) {
@@ -31,3 +31,40 @@ function howMuchCandy(weather, tempRange, avgKids) {
 console.log(howMuchCandy("clear", "50s", 250));
 console.log(howMuchCandy("full moon", "40s", 120));
 console.log(howMuchCandy("rainy", "70s", 180));
+
+
+/* 
+  Solution 2 with case statements
+*/
+
+function howMuchCandy2(weather, tempRange, avgKids) {
+
+  let adjustedKids = avgKids;
+  
+  // Weather adjustment
+  switch (weather) {
+    case "clear":
+      adjustedKids += avgKids * 0.10;
+    case "rainy":
+      adjustedKids -= avgKids * 0.25;
+    case "full moon":
+      adjustedKids += avgKids * 0.25;
+  } 
+
+  // Temperature adjustment
+  switch (tempRange) {
+    case "40s":
+      adjustedKids -= avgKids * 0.05;
+    case "60s":
+      adjustedKids += avgKids * 0.05;
+    case "70s":
+      adjustedKids += avgKids * 0.20;
+  } 
+
+  return adjustedKids * 3;
+  
+}
+
+console.log(howMuchCandy2("clear", "50s", 250));
+console.log(howMuchCandy2("full moon", "40s", 120));
+console.log(howMuchCandy2("rainy", "70s", 180));
