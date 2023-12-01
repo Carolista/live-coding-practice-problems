3b76b57ed5ac0798c5535fc11c94c1e8eab4660a
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 17:07:26 -0500

    added solution for find-highest-num

--- beginner/find-highest-num/solution.js
+++ beginner/find-highest-num/solution.js
@@ -1,3 +1,54 @@
 /* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
+  Solution 1
 */
+
+let testNums1 = [3, -1, 9, 2, -4, 7, 6, 1, 5];
+let testNums2 = [1];
+let testNums3 = [-62, -79, -44, -22, -88];
+
+function findHighestNum(numArr) {
+  let highestNumIndex = 0;
+  for (let i = 1; i < numArr.length; i++) {
+    if (numArr[i] > numArr[highestNumIndex]) {
+      highestNumIndex = i;
+    }
+  }
+  return highestNumIndex;
+}
+
+console.log(findHighestNum(testNums1));
+console.log(findHighestNum(testNums2));
+console.log(findHighestNum(testNums3));
+
+
+/* 
+  Solution 2
+*/
+
+function findHighestNum2(numArr) {
+  let highestNum = numArr[0];
+  for (let i = 1; i < numArr.length; i++) {
+    if (numArr[i] > highestNum) {
+      highestNum = numArr[i];
+    }
+  }
+  return numArr.indexOf(highestNum);
+}
+
+console.log(findHighestNum2(testNums1));
+console.log(findHighestNum2(testNums2));
+console.log(findHighestNum2(testNums3));
+
+
+/* 
+  Solution 3 - Advanced syntax with Math object and spread operator
+*/
+
+function findHighestNum3(numArr) {
+  let highestNum = Math.max(...numArr);
+  return numArr.indexOf(highestNum);
+}
+
+console.log(findHighestNum3(testNums1));
+console.log(findHighestNum3(testNums2));
+console.log(findHighestNum3(testNums3));
