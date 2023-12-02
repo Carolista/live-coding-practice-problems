44456ee69154b657dd5593852dd1b6b81912a6f8
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 23:03:59 -0500

    added solution for longest-repeat

--- beginner/longest-repeat/solution.js
+++ beginner/longest-repeat/solution.js
@@ -1,3 +1,34 @@
 /* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
+  Solution
 */
+
+let testStr1 = "aaa";
+let testStr2 = "a";
+let testStr3 = "bdsagbgagggaaatttyyyyau";
+let testStr4 = "abcdefghijklmnopqrstuvwxyz";
+let testStr5 = "";
+
+function findLongestRepeat(str) {
+  let longest = 1;
+  let currCount = 1;
+  if (str.length < 2) {
+    return str.length; // 0 or 1
+  }
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      currCount++;
+      if (currCount > longest) {
+        longest = currCount;
+      }
+    } else {
+      currCount = 1;
+    }
+  }
+  return longest;
+}
+
+console.log(findLongestRepeat(testStr1));
+console.log(findLongestRepeat(testStr2));
+console.log(findLongestRepeat(testStr3));
+console.log(findLongestRepeat(testStr4));
+console.log(findLongestRepeat(testStr5));
