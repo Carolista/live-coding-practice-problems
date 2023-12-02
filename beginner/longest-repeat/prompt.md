d39c0ed32587d83a4618d73e77e709806d419b27
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 23:44:22 -0500

    added solution for word-count

--- beginner/longest-repeat/prompt.md
+++ beginner/longest-repeat/prompt.md
@@ -3,6 +3,7 @@
 #### Instructions to Interviewee
 Write a function that accepts a string of alphabetic characters as input. It should return an integer equal to the longest length of a substring of consecutive identical characters.
 
+---
 ### Hints and Clarifications
 - Consider edge cases like an empty string or single character
 
--- beginner/word-count/solution.js
+++ beginner/word-count/solution.js
@@ -1,3 +1,44 @@
 /* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
+  Solution 1 (this is clunky but it works)
 */
+
+let testPhrase1 = "One giant leap for mankind";
+let testPhrase2 = "a";
+let testPhrase3 = "";
+let testPhrase4 = "   ";
+
+function countWords(phrase) {
+  if (phrase.length === 0) {
+    return 0;
+  }
+  let spaceCount = 0;
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] === " ") {
+      spaceCount++;
+    }
+  }
+  if (spaceCount === phrase.length) {
+    return 0;
+  }
+  return spaceCount + 1;
+}
+
+console.log(countWords(testPhrase1));
+console.log(countWords(testPhrase2));
+console.log(countWords(testPhrase3));
+console.log(countWords(testPhrase4));
+
+
+/* 
+  Solution 2 - String methods and modern syntax;
+*/
+
+function countWords2(phrase) {
+  if (phrase.trim().length === 0) return 0;
+  return phrase.split(" ").length;
+}
+
+console.log(countWords2(testPhrase1));
+console.log(countWords2(testPhrase2));
+console.log(countWords2(testPhrase3));
+console.log(countWords2(testPhrase4));
