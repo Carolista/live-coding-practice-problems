24b11ec0a7b2ece750da549677cb2ec75882bcfa
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 21:31:30 -0500

    added solution for jennys-number

--- beginner/jennys-number/solution.js
+++ beginner/jennys-number/solution.js
@@ -1,3 +1,27 @@
 /* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
+  Solution 1
 */
+
+let jenny = "j02en8 9nys-n1u m80b e3r!";
+
+let digits = "";
+for (let i = 1; i < jenny.length; i++) {
+  if (!isNaN(jenny[i]) && jenny[i] !== " ") {
+    digits += jenny[i];
+  }
+}
+let calc = String(Number(digits) * 3 - 100);
+let result = calc.slice(0, 3) + "-" + calc.slice(3);
+
+console.log(result);
+
+
+/*
+  Solution 2 - More concise... but perhaps less readable?
+*/
+
+let digitsOnly = jenny.split("").filter(char => !isNaN(parseInt(char))).join("");
+let newNum = String(Number(digitsOnly) * 3 - 100);
+let hyphenated = `${newNum.slice(0, 3)}-${newNum.slice(3)}`;
+
+console.log(hyphenated);
