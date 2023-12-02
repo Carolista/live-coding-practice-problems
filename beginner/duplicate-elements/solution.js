218e476ef95dff3a069989034e825798e858d7a3
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-02 01:20:03 -0500

    added duplicate-elements problem

---
+++ beginner/duplicate-elements/solution.js
@@ -0,0 +1,35 @@
+/* 
+  Solution 1
+*/
+
+let testNums1 = [3, -1, 9, 2, -4, 7, 3, 6, 1, 5];
+let testNums2 = [1, 9];
+let testNums3 = [1, 2, 2, 3];
+
+function hasDuplicates(numArr) {
+  let found = [];
+  for (let i = 0; i < numArr.length; i++) {
+    if (found.includes(numArr[i])) {
+      return true;
+    }
+    found.push(numArr[i]);
+  }
+  return false;
+}
+
+console.log(hasDuplicates(testNums1));
+console.log(hasDuplicates(testNums2));
+console.log(hasDuplicates(testNums3));
+
+
+/* 
+  Solution 2
+*/
+
+function hasDuplicates2(numArr) {
+  return numArr.length !== Array.from(new Set(numArr)).length;
+}
+
+console.log(hasDuplicates2(testNums1));
+console.log(hasDuplicates2(testNums2));
+console.log(hasDuplicates2(testNums3));
