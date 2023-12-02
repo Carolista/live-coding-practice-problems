c449a6d9c7c8848805308b49c140b82aa781373f
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-01 22:08:16 -0500

    added solution for letter-usage

--- beginner/letter-usage/solution.js
+++ beginner/letter-usage/solution.js
@@ -1,3 +1,36 @@
 /* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
+  Solution 1
 */
+
+let testStr = "St. Louis, Kansas City, and Philadelphia";
+
+function countCharacters(str) {
+  let counts = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (counts[char] === undefined) {
+      counts[char] = 1;
+    } else {
+      counts[char]++;
+    }
+  }
+  return counts;
+}
+
+console.log(countCharacters(testStr));
+
+
+/*
+  Solution 2 - Modern syntax
+*/
+
+function countCharacters2(str) {
+  let counts = {};
+  str.toLowerCase().split("").forEach(char => {
+    let isNewChar = counts[char] === undefined;
+    counts[char] = isNewChar ? 1 : counts[char] + 1;
+  });
+  return counts;
+}
+
+console.log(countCharacters2(testStr));
